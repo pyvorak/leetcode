063657f38337f7af6bba24d57b3477dbd3467f0f
BLTech-py <85914538+BLTech-py@users.noreply.github.com>
2024-08-31 10:48:06 +0800

    Update 1514.py

--- 1514/1514.py
+++ 1514/1514.py
@@ -1,20 +1,16 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], probs: List[float], start: int, end: int) -> float:
-        adj = [[] for _ in range(n)]
+        dp = [0] * n
+        dp[start] = 1
 
-        for i, (u, v) in enumerate(edges):
-            adj[u].append([v, probs[i]])
-            adj[v].append([u, probs[i]])
+        while n:
+            valid = False
+            for i, (u, v) in enumerate(edges):
+                p = probs[i]
+                p1, p2 = dp[u] * p, dp[v] * p
+                if p1 > dp[v]: dp[v] = p1; valid = True
+                if p2 > dp[u]: dp[u] = p2; valid = True
+            if not valid: break
+            n -= 1
 
-        dp = [0 for _ in range(n)]
-        pq = [[-1, start]]
-
-        while pq:
-            p, u = heappop(pq)
-            if u == end: break
-            for v, q in adj[u]:
-                if (p2 := p * q) < dp[v]:
-                    dp[v] = p2
-                    heappush(pq, [p2, v])
-
-        return -dp[end]
+        return dp[end]
