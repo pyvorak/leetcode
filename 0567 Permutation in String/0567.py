fbf3f42d58be0ab60e6d08efcb46e019b62ac895
BLTech-py <85914538+BLTech-py@users.noreply.github.com>
2024-10-05 12:10:22 +0800

    Update 0567.py

--- 0567 Permutation in String/0567.py
+++ 0567 Permutation in String/0567.py
@@ -3,20 +3,20 @@ class Solution:
         if len(s2) < len(s1):
             return False
         else:
-            v1, v2 = [0]*26, [0]*26
+            v = [0]*26
 
             for i in range(len(s1)):
-                v1[ord(s1[i]) - ord('a')] += 1
-                v2[ord(s2[i]) - ord('a')] += 1
+                v[ord(s1[i]) - ord('a')] -= 1
+                v[ord(s2[i]) - ord('a')] += 1
             
-            if v1 == v2:
+            if set(v) == {0}:
                 return True
             else:
                 for i in range(len(s1), len(s2)):
-                    v2[ord(s2[i]) - ord('a')] += 1
-                    v2[ord(s2[i-len(s1)]) - ord('a')] -= 1
+                    v[ord(s2[i]) - ord('a')] += 1
+                    v[ord(s2[i-len(s1)]) - ord('a')] -= 1
                     
-                    if v1 == v2:
+                    if set(v) == {0}:
                         return True
 
                 return False
