a6d73b1466c1d7ec89e4b51681adf3e5ba9df473
BLTech-py <85914538+BLTech-py@users.noreply.github.com>
2024-10-08 11:20:12 +0800

    Update 1963.cpp

--- 1963 Minimum Number of Swaps to Make the String Balanced/1963.cpp
+++ 1963 Minimum Number of Swaps to Make the String Balanced/1963.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     int minSwaps(string s) {
         int ans = 0;
-        int l = 0;
-        int r = 0;
 
-        for (char c: s){
-            if (c == ']'){
-                if (r == l){
-                    l++;
-                    ans++;
-                } else {
-                    r++;
-                }
+        for (char c : s) {
+            if (c == '[') {
+                ans++;
             } else {
-                l++;
+                if (ans > 0) {
+                    ans--;
+                }
             }
         }
 
-        return ans;
+        return (ans + 1) / 2;
     }
 };
