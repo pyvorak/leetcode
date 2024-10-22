09db3c2455be2668799b21bde1536d0ffb7df511
BLTech-py <85914538+BLTech-py@users.noreply.github.com>
2024-10-22 22:08:46 +0800

    Create 0783.py

---
+++ 0783 Minimum Distance Between BST Nodes/0783.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        v = []
+
+        def inorder(cur):
+            if cur.left: inorder(cur.left)
+            v.append(cur.val)
+            if cur.right: inorder(cur.right)
+
+        inorder(root)
+
+        ans = v[-1] - v[0]
+
+        for i in range(1, len(v)):
+            ans = min(ans, v[i] - v[i-1])
+
+        return ans        
