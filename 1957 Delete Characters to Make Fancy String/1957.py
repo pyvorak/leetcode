ae2132d0c993523e09261d25ba59754224276b62
BLTech-py <85914538+BLTech-py@users.noreply.github.com>
2024-11-01 12:44:58 +0800

    Update 1957.py

--- 1957 Delete Characters to Make Fancy String/1957.py
+++ 1957 Delete Characters to Make Fancy String/1957.py
@@ -1,14 +1,9 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        if len(s) <= 2:
-            return s
-        else:
-            ans = s[:2]
+        ans = ""
 
-            for c in s[2:]:
-                if c == ans[-1] and c == ans[-2]:
-                    continue
-                else:
-                    tans += c
+        for c in s:
+            if not (len(ans) >= 2 and c == ans[-1] and c == ans[-2]):
+                ans += c
 
-            return ans
+        return ans
