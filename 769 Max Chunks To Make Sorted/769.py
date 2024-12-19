1f7a1d8e83e2eac35ece4b5496093652957e5ad7
BLTech-py <85914538+BLTech-py@users.noreply.github.com>
2024-12-19 17:37:18 +0800

    Update 769.py

--- 769 Max Chunks To Make Sorted/769.py
+++ 769 Max Chunks To Make Sorted/769.py
@@ -1,10 +1,12 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         i, chunks, max_element = 0, 0, 0
-        
-        for i in range(len(arr)):
+        n = len(arr)
+    
+        for i in range(n):
             max_element = max(max_element, arr[i])
             if i == max_element:
                 chunks += 1
 
         return chunks
+
