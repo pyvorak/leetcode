b32970b6af69e4aa61193953ad53fea17d2be2dd
pyvorak <85914538+pyvorak@users.noreply.github.com>
2025-02-01 15:44:58 +0800

    Update 3151.py

--- 3151 Special Array I/3151.py
+++ 3151 Special Array I/3151.py
@@ -1,11 +1,3 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        prev = nums[0] & 1
-
-        for num in nums[1:]:
-            cur = num & 1
-            if cur == prev:
-                return False
-            prev ^= 1
-
-        return True
+        return (n := len(nums)) == 1 or all(nums[i] & 1 != nums[i+1] & 1 for i in range(n-1))
