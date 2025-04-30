17f094d7801872165748a343cae240bb2127ffd1
pyvorak <85914538+pyvorak@users.noreply.github.com>
2025-04-30 22:06:30 +0800

    Update 1920.py

--- 1920 Build Array from Permutation/1920.py
+++ 1920 Build Array from Permutation/1920.py
@@ -1,3 +1,13 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        return [nums[nums[i]] for i in range(len(nums))]
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                j = i
+                first = nums[i]
+                while nums[j] != i:
+                    tmp = nums[j]
+                    nums[j] = -nums[nums[j]]
+                    j = tmp
+                nums[j] = -first
+
+        return [-x for x in nums]
