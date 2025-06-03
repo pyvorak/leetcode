6685c35e49170c39cdc7b7068ad07d5cfaf17d18
pyvorak <85914538+pyvorak@users.noreply.github.com>
2025-06-03 12:39:24 +0800

    Update 1298.py

--- 1298 Maximum Candies You Can Get from Boxes/1298.py
+++ 1298 Maximum Candies You Can Get from Boxes/1298.py
@@ -1,18 +1,18 @@
 class Solution:
     def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
         q = deque(initialBoxes)
+        valid = set([i for i in range(len(status)) if status[i] == 1])
         ans = cnt = 0
-        
+    
         while q and cnt < len(q):
             cur = q.popleft()
-            if status[cur] == 0:
-                q.append(cur)
-                cnt += 1
-            else:
+            if cur in valid:
                 cnt = 0
                 ans += candies[cur]
                 q.extend(containedBoxes[cur])
-                for k in keys[cur]:
-                    status[k] = 1
+                valid.update(keys[cur])    
+            else:
+                q.append(cur)
+                cnt += 1
 
         return ans
